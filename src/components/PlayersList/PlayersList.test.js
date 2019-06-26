2e696563038addf717c6905b0a753d017b491a19
Magdalena arti Zachara <magdalena.j.zachara@gmail.com>
2019-06-26 18:36:08 +0200

    Add PlayersList component and tests

---
+++ src/components/PlayersList/PlayersList.test.js
@@ -0,0 +1,50 @@
+import PlayersList from "./PlayersList";
+import React from "react";
+import { shallow } from "enzyme";
+import Player from "../Player/Player";
+
+it("renders without crashing", () => {
+  shallow(<PlayersList players={[]} />);
+});
+
+it("renders correct number of players", () => {
+  const players = [
+    {
+      name: "Kunegunda",
+      score: 5
+    },
+    {
+      name: "Antoś",
+      score: 0
+    }
+  ];
+  const playerComponent = shallow(<PlayersList players={players} />);
+  console.log(playerComponent.debug());
+  const expectedPlayersNumber = playerComponent.find(Player).length; // not li as it is shallow, Player imported
+
+  expect(expectedPlayersNumber).toEqual(2);
+});
+
+it("calls onScoreUpdate with correct values", () => {
+  const players = [
+    {
+      name: "Kunegunda",
+      score: 5
+    },
+    {
+      name: "Antoś",
+      score: 0
+    }
+  ];
+
+  const mockedOnScoreUpdate = jest.fn();
+  const playerComponent = shallow(
+    <PlayersList players={players} onScoreUpdate={mockedOnScoreUpdate} />
+  );
+
+  const firstPlayer = playerComponent.find(Player).first();
+  const onPlayerScoreChange = firstPlayer.prop("onPlayerScoreChange");
+
+  onPlayerScoreChange(12);
+  expect(mockedOnScoreUpdate).toBeCalledWith(0, 12); // first player, 12 points change
+});
