0524905e405e77c74a62c2f0696bd0b65e8ea931
Magdalena arti Zachara <magdalena.j.zachara@gmail.com>
2019-06-27 09:31:25 +0200

    Modify App to add PlayersList and function updating score with tests

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
 import React, { Component } from "react";
+import PlayersList from "./components/PlayersList/PlayersList";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      players: [
+        {
+          name: "Kunegunda",
+          score: 5
+        },
+        {
+          name: "Antoś",
+          score: 0
+        }
+      ]
+    };
+  }
+  // with arrow function as @babel/plugin-proposal-class-properties is installed
+  onScoreUpdate = (playerIndex, scoreChange) => {
+    this.setState({
+      players: this.state.players.map((player, index) => {
+        if (index === playerIndex) {
+          return { ...player, score: player.score + scoreChange };
+        }
+        return player;
+      })
+    });
+  };
+
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <PlayersList
+          players={this.state.players}
+          onScoreUpdate={this.onScoreUpdate}
+        />
+      </div>
+    );
   }
 }
 
